15a0ae75f962356bdb3df60ba8f1824de61722aa
David Charboneau <david@thecharboneaus.net>
2013-04-16 19:35:46 -0400

    merge

--- src/ggrc/models/categories.py
+++ src/ggrc/models/categories.py
@@ -1,17 +1,12 @@
 from ggrc import db
+from .mixins import Base, Child
 
-class Category(db.Model):
+class Category(Base, Child):
   __tablename__ = 'categories'
 
-  id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String)
-  parent_id = db.Column(db.Integer)
   lft = db.Column(db.Integer)
   rgt = db.Column(db.Integer)
   scope_id = db.Column(db.Integer)
   depth = db.Column(db.Integer)
-  created_at = db.Column(db.DateTime, server_default=db.text('current_timestamp'))
-  updated_at = db.Column(db.DateTime, server_onupdate=db.text('current_timestamp'))
-  modified_by_id = db.Column(db.Integer)
   required = db.Column(db.Boolean)
-
