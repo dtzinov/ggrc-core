7e4da258be33c9a57146dc2d0c70cf388b050328
David Charboneau <david@thecharboneaus.net>
2013-04-30 14:41:22 -0400

    Fix for Flask reloading - make sure that the path is clean. This needs to be fixed, but this hack will do for the short term

--- src/ggrc/__main__.py
+++ src/ggrc/__main__.py
@@ -1,6 +1,10 @@
 if __name__ == "__main__" and (__package__ is None or __package__ == ""):
   import os, sys
   parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+  if sys.path[0].endswith('ggrc'):
+    # FIXME Not sure how this is getting in there, but it happens on Flask
+    # restart!
+    del sys.path[0]
   sys.path.insert(0, parent_dir)
   import ggrc
   __package__ = "ggrc"
