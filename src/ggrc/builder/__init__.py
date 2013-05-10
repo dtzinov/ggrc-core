5ea9a15ebd725e9f30cb00842ccd5b0bf926b402
David Charboneau <david@thecharboneaus.net>
2013-05-10 10:09:16 -0400

    Adding docstring comments.

--- src/ggrc/builder/__init__.py
+++ src/ggrc/builder/__init__.py
@@ -1,1 +1,5 @@
+'''Resource state representation handlers for gGRC models. Builder modules will
+produce specific resource state representations for gGRC models as well as
+update/create gGRC model instances from resource state representations.
+'''
 from .json import *
--- src/ggrc/builder/json.py
+++ src/ggrc/builder/json.py
@@ -1,3 +1,4 @@
+'''JSON resource state representation handler for gGRC models.'''
 import ggrc.builder
 import ggrc.services
 from datetime import datetime
@@ -13,17 +14,26 @@ def url_for(obj):
   return service.url_for(obj) if service else None
 
 def get_json_builder(obj):
+  '''Instantiate or retrieve a JSON representation builder for the given
+  object.
+  '''
   if type(obj) is type:
     cls = obj
   else:
     cls = obj.__class__
+  # Lookup the builder instance in the builder module
   builder = getattr(ggrc.builder, cls.__name__, None)
   if not builder:
+    # Create the builder and cache it in the builder module
     builder = Builder(cls)
     setattr(ggrc.builder, cls.__name__, builder)
   return builder
 
 def publish(obj):
+  '''Translate ``obj`` into a valid JSON value. Objects with properties are
+  translated into a ``dict`` object representing a JSON object while simple
+  values are returned unchanged or specially formatted if needed.
+  '''
   publisher = get_json_builder(obj)
   if publisher and hasattr(publisher, '_publish_attrs') \
       and publisher._publish_attrs:
@@ -35,6 +45,10 @@ def publish(obj):
   return obj
 
 def update(obj, json_obj):
+  '''Translate the state represented by ``json_obj`` into update actions
+  performed upon the model object ``obj``. After performing the update ``obj``
+  and ``json_obj`` should be equivalent representations of the model state.
+  '''
   updater = get_json_builder(obj)
   if updater:
     updater.update(obj, json_obj)
@@ -42,17 +56,33 @@ def update(obj, json_obj):
   #Nothing, perhaps log, assume omitted by design
 
 def create(obj, json_obj):
+  '''Translate the state represented by ``json_obj`` into update actions
+  performed upon the new model object ``obj``. After performing the update
+  ``obj`` and ``json_obj`` should be equivalent representations of the model
+  state.
+  '''
   creator = get_json_builder(obj)
   if creator:
     creator.create(obj, json_obj)
 
 class UpdateAttrHandler(object):
+  '''Performs the translation of a JSON state representation into update
+  actions performed on a model object instance.
+  '''
   @classmethod
   def do_update_attr(cls, obj, json_obj, attr):
+    '''Perform the update to ``obj`` required to make the attribute attr
+    equivalent in ``obj`` and ``json_obj``.
+    '''
     if (hasattr(attr, '__call__')):
+      # The attribute has been decorated with a callable, grab the name and
+      # invoke the callable to get the value
       attr_name = attr.attr_name
       value = attr(cls, obj, json_obj)
     else:
+      # Lookup the method to use to perform the update. Use reflection to
+      # key off of the type of the attribute and invoke the method of the
+      # same name.
       attr_name = attr
       class_attr = getattr(obj.__class__, attr_name)
       method = getattr(cls, class_attr.__class__.__name__)
@@ -61,11 +91,13 @@ class UpdateAttrHandler(object):
 
   @classmethod
   def InstrumentedAttribute(cls, obj, json_obj, attr_name, class_attr):
+    '''Translate the JSON value for an ``InstrumentedAttribute``'''
     method = getattr(cls, class_attr.property.__class__.__name__)
     return method(obj, json_obj, attr_name, class_attr)
 
   @classmethod
   def ColumnProperty(cls, obj, json_obj, attr_name, class_attr):
+    '''Translate the JSON value for a ``ColumnProperty``'''
     method = getattr(
         cls,
         class_attr.property.expression.type.__class__.__name__,
@@ -74,21 +106,26 @@ class UpdateAttrHandler(object):
 
   @classmethod
   def default_column_handler(cls, obj, json_obj, attr_name, class_attr):
+    '''Translate the JSON value for a simple value column'''
     return json_obj.get(attr_name)
 
   @classmethod
   def DateTime(cls, obj, json_obj, attr_name, class_attr):
+    '''Translate the JSON value for a ``Datetime`` column.'''
     value = json_obj.get(attr_name)
     return parse_date(value) if value else None
 
   @classmethod
   def Date(cls, obj, json_obj, attr_name, class_attr):
+    '''Translate the JSON value for a ``Date`` column.'''
     value = json_obj.get(attr_name)
     return datetime.strptime(value, "%Y-%m-%d") if value else None
 
   @classmethod
   def query_for(cls, rel_class, json_obj, attr_name, uselist):
+    '''Resolve the model object instance referred to by the JSON value.'''
     if uselist:
+      # The value is a collection of links, resolve the collection of objects
       value = json_obj.get(attr_name)
       rel_ids = [o[u'id'] for o in value] if value else []
       if rel_ids:
@@ -105,17 +142,22 @@ class UpdateAttrHandler(object):
 
   @classmethod
   def RelationshipProperty(cls, obj, json_obj, attr_name, class_attr):
+    '''Translate the JSON value for a ``RelationshipProperty``.'''
     rel_class = class_attr.property.mapper.class_
     return cls.query_for(
         rel_class, json_obj, attr_name, class_attr.property.uselist)
 
   @classmethod
   def AssociationProxy(cls, obj, json_obj, attr_name, class_attr):
+    '''Translate the JSON value for an ``AssociationProxy``.'''
     rel_class = class_attr.remote_attr.property.mapper.class_
     return cls.query_for(rel_class, json_obj, attr_name, True)
 
   @classmethod
   def property(cls, obj, json_obj, attr_name, class_attr):
+    '''Translate the JSON value for an object method decorated as a
+    ``property``.
+    '''
     #FIXME need a way to decide this. Require link? Use URNs?
     #  reflective approaches won't work as this is used for polymorphic
     #  properties
@@ -125,32 +167,34 @@ class UpdateAttrHandler(object):
 
 
 class Builder(AttributeInfo):
-  '''JSON Dictionary builder for ggrc.models.* objects and their mixins.
+  '''JSON Dictionary builder for ggrc.models.* objects and their mixins.'''
 
-  Builder classes can override the `_simple_attrs` class attribute with a list
-  of attribute names to be extracted from a model instance and included into
-  the dictionary for the json representation of the object.
+  def generate_link_object_for(self, obj):
+    return {'id': obj.id, 'href': url_for(obj)}
 
-  Builder classes can also override the `_build_contribution` class method.
-
-  A Builder class should be defined for __every__ ggrc.model class and mixin.
-  Mixin inheritance is respected - the base classes will be walked and any
-  Builder for that class will be run and have it's contributions to the
-  dictionary recorded.
-  '''
   def publish_link_collection(self, obj, json_obj, attr_name):
+    '''The ``attr_name`` attribute is a collection of object references;
+    translate the collection of object references into a collection of link
+    objects for the JSON dictionary representation.
+    '''
     # FIXME: This should not require `if o is not None`
     # - if `o is None`, it means a join pointed to a non-existent object
-    return [{'id': o.id, 'href': url_for(o)}
+    return [self.generate_link_object_for(o) \
         for o in getattr(obj, attr_name) if o is not None]
 
   def publish_link(self, obj, json_obj, attr_name):
+    '''The ``attr_name`` attribute is an object reference; translate the object
+    reference into a link object for the JSON dictionary representation.
+    '''
     attr_value = getattr(obj, attr_name)
     if attr_value:
-      return {'id': attr_value.id, 'href': url_for(attr_value)}
+      return self.generate_link_object_for(attr_value)
     return None
 
   def publish_attrs(self, obj, json_obj):
+    '''Translate the state represented by ``obj`` into the JSON dictionary
+    ``json_obj``.
+    '''
     for attr in self._publish_attrs:
       if hasattr(attr, '__call__'):
         attr_name = attr.attr_name
@@ -174,22 +218,39 @@ class Builder(AttributeInfo):
 
   @classmethod
   def do_update_attrs(cls, obj, json_obj, attrs):
+    '''Translate every attribute in ``attrs`` from the JSON dictionary value
+    to a value or model object instance for references set for the attribute
+    in ``obj``.
+    '''
     for attr_name in attrs:
       UpdateAttrHandler.do_update_attr(obj, json_obj, attr_name)
 
   def update_attrs(self, obj, json_obj):
+    '''Translate the state representation given by ``json_obj`` into the
+    model object ``obj``.
+    '''
     self.do_update_attrs(obj, json_obj, self._update_attrs)
 
   def create_attrs(self, obj, json_obj):
+    '''Translate the state representation given by ``json_obj`` into the new
+    model object ``obj``.
+    '''
     self.do_update_attrs(obj, json_obj, self._create_attrs)
 
   def publish_contribution(self, obj):
+    '''Translate the state represented by ``obj`` into a JSON dictionary'''
     json_obj = {}
     self.publish_attrs(obj, json_obj)
     return json_obj
 
   def update(self, obj, json_obj):
+    '''Update the state represented by ``obj`` to be equivalent to the state
+    represented by the JSON dictionary ``json_obj``.
+    '''
     self.update_attrs(obj, json_obj)
 
   def create(self, obj, json_obj):
+    '''Update the state of the new model object ``obj`` to be equivalent to the
+    state represented by the JSON dictionary ``json_obj``.
+    '''
     self.create_attrs(obj, json_obj)
